81ea0dd1517314696787c7edde74590d6b682684
fabiotorres95 <engfabiotorres@outlook.com>
2023-03-13 16:44:58 -0300

    login elements created, input verification enables button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>Hello, TrybeWallet!</div>
+    <Route path="/" component={ Login } />
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,61 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.verify = this.verify.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+      isBtnDisabled: true,
+    };
+  }
+
+  checkInfo = ({ target }) => {
+    if (target.type === 'email') {
+      this.setState(() => ({ email: target.value }), this.verify);
+    } else {
+      this.setState(() => ({ password: target.value }), this.verify);
+    }
+  };
+
+  verify = () => {
+    const { email, password } = this.state;
+    let result = true;
+    const size = 6;
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (password.length >= size && email.match(emailRegex)) {
+      result = false;
+    }
+
+    this.setState({ isBtnDisabled: result });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { isBtnDisabled } = this.state;
+    return (
+      <>
+        <div>Login</div>
+        <label>
+          Email:
+          <input
+            type="email"
+            data-testid="email-input"
+            onChange={ this.checkInfo }
+          />
+        </label>
+        <label>
+          Senha:
+          <input
+            type="text"
+            data-testid="password-input"
+            onChange={ this.checkInfo }
+          />
+        </label>
+        <button disabled={ isBtnDisabled }>Entrar</button>
+      </>
+    );
   }
 }
 
